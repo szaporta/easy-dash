90b23578e235936810ed78c9d2c9024587543eec
aalbertson <arlingtonalbertson@gmail.com>
2019-03-29 15:03:43 -0700

    adding args parsing

--- easy_dash.py
+++ easy_dash.py
@@ -1,19 +1,6 @@
 from datadog import initialize, api
 import argparse
 
-# parser = argparse.ArgumentParser()
-# group_origin = parser.add_argument_group('Keys for Origininating DD account (where the dashboard lives)')
-# group_origin.add_argument('--origapikey', help='Enter the origin API key')
-# group_origin.add_argument('--origappkey', help='Enter the origin APP key')
-# group_dest = parser.add_argument_group('Keys for the Destination DD account (where the dashboard is going')
-# group_dest.add_argument('--destapikey', help='Enter the destination API key')
-# group_dest.add_argument('--destappkey', help='Enter the destination APP key')
-# group_dashboard = parser.add_argument_group('Dashboard to copy')
-# group_dashboard.add_argument('--dash', help='ID of the dashboard you want to clone ex: 442-3kr-k68')
-# args = parser.parse_args()
-
-
-
 def get_original_dashboard(api_key, app_key, dashboard_id):
 
     options = { 'api_key': api_key, 'app_key': app_key }
@@ -57,29 +44,7 @@ if __name__ == '__main__':
     parser.add_argument('--destapikey', help='Enter the destination API key')
     parser.add_argument('--destappkey', help='Enter the destination APP key')
     parser.add_argument('--dash', help='ID of the dashboard you want to clone ex: 442-3kr-k68')
-
-
-#     parser.add_argument('square', help='displace a square of a given number', type=int)
-#     parser.add_argument("x", type=int, help="the base")
-#     parser.add_argument("y", type=int, help="the exponent")
-#     parser.add_argument('-v', '--verbosity', help='increase output verbosity', action='count', default=0)
     args = parser.parse_args()
-#     answer = args.x**args.y
-#     if args.verbosity >= 2:
-#             print("{} to the power {} equals {}".format(args.x, args.y, answer))
-#     elif args.verbosity >= 1:
-#             print("{}^{} == {}".format(args.x, args.y, answer))
-#     else:
-#             print(answer)
-#     for arg in args:
-#             print("Arg: ", arg)
-    print(args)
-    print(args.dash)
-    print(args.origapikey)
-    print(args.origappkey)
-    print(args.destapikey)
-    print(args.destappkey)
-
 
     dashboard = get_original_dashboard(
         
